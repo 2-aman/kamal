4a81223f6da116686567bd74a8f988bdb35a8860
2-aman <69178363+2-aman@users.noreply.github.com>
2020-08-26 00:56:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+// nav bar
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
+//   end of nav
+
+// gallery
+
+ 
+ 
+
+  //  rnf gallery
